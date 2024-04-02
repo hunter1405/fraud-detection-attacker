b0839778f3f7b928d667882ceef1c5f8b0c1aab3
Nguyen Tuan Hung <91263297+hunter1405@users.noreply.github.com>
2024-04-02 23:02:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,11 +83,6 @@ def main():
 
     else:
         # Multi transaction 
-        st.subheader('Please Download the Demo excel file')
-        st.text('Note:- enter the details of Company, save & Upload, Dont change to format.!')
-        # HTML code for downloading demo file 
-        com.html(f"""<button onclick="window.location.href='https://drive.google.com/uc?export=download&id=10aYBUF50jjAWvi-ukZZE2Q6_8pbLoUon';">
-                          Download Demo File</button>""", height=30)
         multi_cust(sl.file_uploader('Please Upload Excel file'))
 
 def multi_cust(file):
